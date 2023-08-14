f4cca9dd2ec59e6312e37b2d69814e982d28e5b1
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2023-08-14 12:20:07 -0300

    feat: add routes and animations

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,26 @@
+import {FiLink } from 'react-icons/fi';
+import './home.css';
 
 export default function Home(){
     return(
-      <div>
-        <h1>Pagina Home</h1>
+      <div className="container-home">
+        <div className="logo">
+            <img src="/links.png" alt="My links"/>
+            <h1>My links</h1>
+            <span>Cole o seu link para encurtar 👇</span>
+        </div>
+        <div className="area-input">
+            <div>
+                <FiLink size={24} color="#fff"/>
+                <input
+                    placeholder='Cole o seu link aqui...'
+                />
+            </div>
+
+            <button>
+                Encurtar link
+            </button>
+        </div>
       </div>
     )
   }
