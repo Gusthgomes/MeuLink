fbda441b94c1e973ab615a86ba8b9119fb2dbcc9
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2023-08-15 12:29:46 -0300

    feat: created page links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,8 +1,39 @@
+import { Link } from 'react-router-dom'
+import './links.css';
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
 
-export default function Link(){
+export default function Links(){
     return(
-      <div>
-        <h1>Pagina Links</h1>
+      <div className='links-container'>
+        
+        <div className='links-header'>
+          <Link to="/">
+            <FiArrowLeft size={38} color="#fff" />
+          </Link>
+          
+          <h1>Meus Links</h1>
+        </div>
+
+        <div className='links-item'>
+          <button className='link'>
+            <FiLink size={18} color="#fff"/>
+            https://google.com
+          </button>
+          <button className='link-delete'>
+            <FiTrash size={24} color="#ff5454" />
+          </button>
+        </div>
+
+        <div className='links-item'>
+          <button className='link'>
+            <FiLink size={18} color="#fff"/>
+            https://google.com
+          </button>
+          <button className='link-delete'>
+            <FiTrash size={24} color="#ff5454" />
+          </button>
+        </div>
+
       </div>
     )
   }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
-import Link from './pages/Links';
+import Links from './pages/Links';
 
 function RoutesApp(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/links" element={<Link/>}/>
+                <Route path="/links" element={<Links/>}/>
             </Routes>
         </BrowserRouter>
     )
