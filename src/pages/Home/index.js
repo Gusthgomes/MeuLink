687d7a86654695490886b29879b86796559cf5b6
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2023-08-17 13:35:17 -0300

    feat: created animations

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import {FiLink } from 'react-icons/fi';
 import './home.css';
 import Menu from '../../components/Menu';
 
 export default function Home(){
+
+    const [link, setLink] = useState("");
+
+    function handleShortLink(){
+        alert("CLICOU" + link);
+    }
+
     return(
       <div className="container-home">
         <div className="logo">
@@ -15,10 +23,12 @@ export default function Home(){
                 <FiLink size={24} color="#fff"/>
                 <input
                     placeholder='Cole o seu link aqui...'
+                    value={link}
+                    onChange={ (e) => setLink(e.target.value) }
                 />
             </div>
 
-            <button>
+            <button onClick={handleShortLink}>
                 Encurtar link
             </button>
         </div>
